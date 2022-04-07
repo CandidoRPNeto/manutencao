f0b5648d38e2dfea3022b8e3206cf4d7f66e4e1d
DaviBrito13 <davipires52@gmail.com>
2022-04-07 20:23:11 -0300

    Criação de Entidade Solicitacao, e do SolicitacaoController. Esperando métodos para persistir.

---
+++ src/main/java/br/ucsal/manutencao/controller/SolicitacaoController.java
@@ -0,0 +1,46 @@
+package br.ucsal.manutencao.controller;
+
+import br.ucsal.manutencao.model.entidades.Solicitacao;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import  br.ucsal.banco.Solicitacoes;
+
+import java.util.ArrayList;
+
+public class SolicitacaoController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarSolicitacao(@RequestBody Solicitacao PostSolicitacao) {
+        Solicitacao solicitacao = new Solicitacao();
+        solicitacao.setNomeSolicitante(PostSolicitacao.getNomeSolicitante());
+        solicitacao.setReclamacao(PostSolicitacao.getReclamacao());
+        // Setar em Array
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarSolicitacao(@PathVariable Integer id) {
+        // Buscar no Array
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarSolicitacao(@PathVariable Integer id, @RequestBody Solicitacao PostSolicitacao) {
+        // Updatar no array
+    }
+
+    @GetMapping
+    public ArrayList<Solicitacao> getAll() {
+        // Retornar array com todas as solicitações
+        ArrayList<Solicitacao> placeholder = new ArrayList<>();
+        return placeholder;
+    }
+
+    @GetMapping("/{nomeSolicitante}")
+    public Solicitacao getSolicitacao(@PathVariable String nomeSolicitante ) {
+        // Retornar solicitacao por nome
+        Solicitacao placeholder = new Solicitacao();
+        return placeholder;
+    }
+
+}
--- src/main/java/br/ucsal/manutencao/model/entidades/Solicitacao.java
+++ src/main/java/br/ucsal/manutencao/model/entidades/Solicitacao.java
@@ -1,5 +1,36 @@
 package br.ucsal.manutencao.model.entidades;
 
+
 public class Solicitacao {
-    
+    private static Integer id;
+    private String nomeSolicitante;
+    private String reclamacao;
+
+    public Solicitacao() {
+        this.id = id++;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeSolicitante() {
+        return nomeSolicitante;
+    }
+
+    public void setNomeSolicitante(String nomeSolicitante) {
+        this.nomeSolicitante = nomeSolicitante;
+    }
+
+    public String getReclamacao() {
+        return reclamacao;
+    }
+
+    public void setReclamacao(String reclamacao) {
+        this.reclamacao = reclamacao;
+    }
 }
